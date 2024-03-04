8c6fb21a22f5db45344a8f2d6c6300ed65eab921
joseph <elajoe24@gmail.com>
2024-03-04 12:00:23 +0000

    modify file

--- db_utils.py
+++ db_utils.py
@@ -2,6 +2,8 @@ import yaml
 import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+
 
 class RDSDatabaseConnector:
     def __init__(self):
@@ -41,18 +43,61 @@ class RDSDatabaseConnector:
         self.data_pandas.to_csv("data_pandas.csv", index=False)
         
         
-    def load_data(self):
-        self.save_data()
+    def load_data(self, saved_file):
+        self.saved_file = "data_pandas.csv"
         self.load_csv = pd.read_csv("data_pandas.csv")
         return self.load_csv
     
+df = RDSDatabaseConnector().load_data(saved_file= "data_pandas.csv")   
+
+
+class DataTransform:
     
-love = RDSDatabaseConnector()
-love.load_data()
-df = love.load_data()
-print(df.head(5))
-
-
-
+    @staticmethod
+    def to_categorical(df, col):
+        df[col] = df[col].astype("category")
+        return df
  
 
+    @staticmethod
+    def TW_timedelta(df, col):
+        df[col] = pd.to_timedelta(df[col], unit='s')
+        return df
+    
+DataTransform().to_categorical(df, col="Type")
+DataTransform().TW_timedelta(df,  col="Tool wear[min]")
+
+class DataFrameInfo:
+    
+    
+    @staticmethod
+    def col_dtypes(df):
+        return df.dtypes
+    
+    @staticmethod
+    def df_info(df):
+        return df.describe()
+    
+    @staticmethod
+    def dnst_cnt(df, col):
+        return df[col].nunique()
+     
+    
+    @staticmethod
+    def count_null(df):
+        count_null_percentage = df.isnull().sum()* 100/len(df)
+        return count_null_percentage
+
+df = DataFrameInfo().col_dtypes()
+df = DataFrameInfo().df_info()
+df = DataFrameInfo().dnst_cnt(col= "Type")
+df = DataFrameInfo().count_null()
+
+     
+#class Plotter:
+    
+    
+    
+    
+#class  DataFrameTransform: 
+     
